9ab601885bf57d9c95fbdb6a910ecec575fe1a43
Sergio Flores <byoigres@gmail.com>
2016-08-13 15:15:02 -0600

    Added error message to Select component

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -10,9 +10,10 @@ class Select
   }
 
   render() {
-    const { items, placeholder, ...props } = this.props;
+    const { items, placeholder, error, ...props } = this.props;
 
     let itemList = null;
+    let errorContainer = null;
 
     if (items !== undefined && items.length > 0) {
       itemList = items.map((item) => (
@@ -25,24 +26,33 @@ class Select
         </option>
       ));
     }
+    console.log('error', error);
+    if (error) {
+      errorContainer = (
+        <div className={styles['select-error']}>{error}</div>
+      );
+    }
 
     return (
-      <div className={styles['select-container']}>
-        <select
-          className={styles['select-element']}
-          {...props}
-          ref="select"
-          defaultValue=""
-        >
-          <option
-            className={styles['select-item-placeholder']}
-            disabled
-            value=""
+      <div className={styles.select}>
+        <div className={styles['select-container']}>
+          <select
+            className={styles['select-element']}
+            {...props}
+            ref="select"
+            defaultValue=""
           >
-            {placeholder}
-          </option>
-          {itemList}
-        </select>
+            <option
+              className={styles['select-item-placeholder']}
+              disabled
+              value=""
+            >
+              {placeholder}
+            </option>
+            {itemList}
+          </select>
+        </div>
+        {errorContainer}
       </div>
     );
   }
@@ -51,6 +61,7 @@ class Select
 Select.propTypes = {
   placeholder: PropTypes.string,
   items: PropTypes.array,
+  error: PropTypes.string,
 };
 
 export default Select;
