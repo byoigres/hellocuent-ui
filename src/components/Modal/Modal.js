3a90fc51b086c5e2a25fcb79447eb55ae03e9937
Sergio Flores <byoigres@gmail.com>
2017-02-24 19:10:22 -0700

    Added ok and cancel text

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,8 +1,13 @@
 import React, { PropTypes } from 'react';
-
+import Button from 'components/Button';
 import styles from 'styles';
 
-const Modal = ({ children, title }) => (
+const Modal = ({
+  children,
+  title,
+  cancelText,
+  okText,
+}) => (
   <div className={styles.modal}>
     <div className={styles['modal-backdrop']}>
       <div className={styles['modal-dialog']}>
@@ -14,7 +19,12 @@ const Modal = ({ children, title }) => (
             {children}
           </div>
           <div className={styles['modal-actions']}>
-            <button>Ok</button>
+            <Button
+              text={cancelText}
+            />
+            <Button
+              text={okText}
+            />
           </div>
         </div>
       </div>
@@ -25,10 +35,14 @@ const Modal = ({ children, title }) => (
 Modal.propTypes = {
   children: PropTypes.node.isRequired,
   title: PropTypes.string,
+  cancelText: PropTypes.string,
+  okText: PropTypes.string,
 };
 
 Modal.defaultProps = {
   title: null,
+  cancelText: 'Cancel',
+  okText: 'OK',
 };
 
 export default Modal;
